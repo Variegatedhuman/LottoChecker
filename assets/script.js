018f61daec2743aaa8056731fb4790d1363d6da0
Adam <116999437+Variegatedhuman@users.noreply.github.com>
2023-01-05 22:36:52 -0600

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+
+var closeBtn = document.querySelector(".closeBtn")
+var welcomeMess = document.querySelector(".welcomeMessage")
+
+closeBtn.addEventListener("click", mainMenu)
+
+function mainMenu() {
+    welcomeMess.classList.add("hide");
+}
+
+  
+
+
+
